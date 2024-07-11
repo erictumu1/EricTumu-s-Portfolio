ada1057b4dc9aa5aa87a5238d05bc02ecda02b85
erictumu1 <erictumu@outlook.com>
2024-07-11 07:42:57 -0400

    updated portfolio

--- script.js
+++ script.js
@@ -4,3 +4,27 @@ function togglemenu(){
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+document.addEventListener("DOMContentLoaded", function() {
+    const faders = document.querySelectorAll('.fadein');
+
+    const appearOptions = {
+        threshold: 0.5,
+        rootMargin: "0px 0px -50px 0px"
+    };
+
+    const appearOnScroll = new IntersectionObserver(function(entries, appearOnScroll) {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('visible');
+            } else {
+                entry.target.classList.remove('visible');
+            }
+        });
+    }, appearOptions);
+
+    faders.forEach(fader => {
+        appearOnScroll.observe(fader);
+    });
+});
+
+  
