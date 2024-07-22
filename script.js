784b0d9561015bdb1b0f23858784ad443862f83e
erictumu1 <erictumu@outlook.com>
2024-07-22 13:22:48 -0400

    Upgrades to the Portfolio and addition of a project

--- script.js
+++ script.js
@@ -5,6 +5,48 @@ function togglemenu(){
     icon.classList.toggle("open");
 }
 document.addEventListener("DOMContentLoaded", function() {
+    const menuItems = document.querySelectorAll('.navbar a');
+    const sections = document.querySelectorAll('section');
+
+    function setActiveLink() {
+        let index = sections.length;
+
+        if (window.scrollY === 0) {
+            index = 0;
+        } else {
+            while (--index && window.scrollY + 50 < sections[index].offsetTop) {}
+        }
+    
+        menuItems.forEach((link) => link.classList.remove('active'));
+        const activeLink = [...menuItems].find((link) => link.getAttribute('href') === `#${sections[index]?.id}`);
+        if (activeLink) {
+            activeLink.classList.add('active');
+        } else if (index === 0) {
+            menuItems[0].classList.add('active');
+        }
+    }
+
+    menuItems.forEach(item => {
+        item.addEventListener('click', function(event) {
+            event.preventDefault();
+
+            const targetId = this.getAttribute('href').substring(1);
+            const targetSection = document.getElementById(targetId);
+            
+            if (targetSection) {
+                targetSection.scrollIntoView({ behavior: 'smooth' });
+                menuItems.forEach(link => link.classList.remove('active'));
+                this.classList.add('active');
+            }
+        });
+    });
+    
+    window.addEventListener('load', setActiveLink);
+    window.addEventListener('scroll', setActiveLink);
+    setActiveLink();
+});
+
+document.addEventListener("DOMContentLoaded", function() {
     const faders = document.querySelectorAll('.fadein');
 
     const appearOptions = {
