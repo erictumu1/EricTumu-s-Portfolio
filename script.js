19d67d770abfc438b96655e6ae0fd780483aeb90
erictumu1 <erictumu@outlook.com>
2025-08-28 15:55:01 -0400

    Addition of Pop Fruity project to the website

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ function togglemenu() {
   icon.classList.toggle("open");
 }
 document.addEventListener("DOMContentLoaded", function () {
-  const menuItems = document.querySelectorAll('.navbar a');
-  const sections = document.querySelectorAll('section');
+  const menuItems = document.querySelectorAll(".navbar a");
+  const sections = document.querySelectorAll("section");
 
   function setActiveLink() {
     let index = sections.length;
@@ -17,75 +17,83 @@ document.addEventListener("DOMContentLoaded", function () {
       while (--index && window.scrollY + 50 < sections[index].offsetTop) { }
     }
 
-    menuItems.forEach((link) => link.classList.remove('active'));
-    const activeLink = [...menuItems].find((link) => link.getAttribute('href') === `#${sections[index]?.id}`);
+    menuItems.forEach((link) => link.classList.remove("active"));
+    const activeLink = [...menuItems].find(
+      (link) => link.getAttribute("href") === `#${sections[index]?.id}`
+    );
     if (activeLink) {
-      activeLink.classList.add('active');
+      activeLink.classList.add("active");
     } else if (index === 0) {
-      menuItems[0].classList.add('active');
+      menuItems[0].classList.add("active");
     }
   }
 
-  menuItems.forEach(item => {
-    item.addEventListener('click', function (event) {
+  menuItems.forEach((item) => {
+    item.addEventListener("click", function (event) {
       event.preventDefault();
 
-      const targetId = this.getAttribute('href').substring(1);
+      const targetId = this.getAttribute("href").substring(1);
       const targetSection = document.getElementById(targetId);
 
       if (targetSection) {
-        targetSection.scrollIntoView({ behavior: 'smooth' });
-        menuItems.forEach(link => link.classList.remove('active'));
-        this.classList.add('active');
+        targetSection.scrollIntoView({ behavior: "smooth" });
+        menuItems.forEach((link) => link.classList.remove("active"));
+        this.classList.add("active");
       }
     });
   });
 
-  window.addEventListener('load', setActiveLink);
-  window.addEventListener('scroll', setActiveLink);
+  window.addEventListener("load", setActiveLink);
+  window.addEventListener("scroll", setActiveLink);
   setActiveLink();
 });
 
 document.addEventListener("DOMContentLoaded", function () {
-  const faders = document.querySelectorAll('.fadein');
+  const faders = document.querySelectorAll(".fadein");
 
   const appearOptions = {
     threshold: 0.5,
-    rootMargin: "0px 0px -50px 0px"
+    rootMargin: "0px 0px -50px 0px",
   };
 
-  const appearOnScroll = new IntersectionObserver(function (entries, appearOnScroll) {
-    entries.forEach(entry => {
+  const appearOnScroll = new IntersectionObserver(function (
+    entries,
+    appearOnScroll
+  ) {
+    entries.forEach((entry) => {
       if (entry.isIntersecting) {
-        entry.target.classList.add('visible');
+        entry.target.classList.add("visible");
       } else {
-        entry.target.classList.remove('visible');
+        entry.target.classList.remove("visible");
       }
     });
-  }, appearOptions);
+  },
+    appearOptions);
 
-  faders.forEach(fader => {
+  faders.forEach((fader) => {
     appearOnScroll.observe(fader);
   });
 });
 document.addEventListener("DOMContentLoaded", function () {
-  const projectLinks = document.querySelectorAll('.article-container article a');
-  const projects = document.querySelectorAll('.article-container article');
+  const projectLinks = document.querySelectorAll(
+    ".article-container article a"
+  );
+  const projects = document.querySelectorAll(".article-container article");
 
-  projectLinks.forEach(link => {
-    link.addEventListener('click', function (event) {
+  projectLinks.forEach((link) => {
+    link.addEventListener("click", function (event) {
       event.preventDefault();
-      projects.forEach(project => project.classList.remove('highlight'));
+      projects.forEach((project) => project.classList.remove("highlight"));
 
-      const targetProjectId = this.getAttribute('href').substring(1);
+      const targetProjectId = this.getAttribute("href").substring(1);
       const targetProject = document.getElementById(targetProjectId);
 
       if (targetProject) {
-        targetProject.classList.add('highlight');
-        targetProject.scrollIntoView({ behavior: 'smooth' });
+        targetProject.classList.add("highlight");
+        targetProject.scrollIntoView({ behavior: "smooth" });
 
         setTimeout(() => {
-          targetProject.classList.remove('highlight');
+          targetProject.classList.remove("highlight");
         }, 10000);
       }
     });
@@ -98,7 +106,7 @@ const navLinks = document.querySelectorAll(".nav-link");
 window.addEventListener("scroll", () => {
   let current = "";
 
-  sections.forEach(section => {
+  sections.forEach((section) => {
     const sectionTop = section.offsetTop;
     const sectionHeight = section.clientHeight;
 
@@ -107,7 +115,7 @@ window.addEventListener("scroll", () => {
     }
   });
 
-  navLinks.forEach(link => {
+  navLinks.forEach((link) => {
     link.classList.remove("active");
     if (link.getAttribute("href") === `#${current}`) {
       link.classList.add("active");
@@ -116,68 +124,90 @@ window.addEventListener("scroll", () => {
 });
 
 //Script to make the page automatically go the top when a user refreshes the page
-window.addEventListener('load', () => {
+window.addEventListener("load", () => {
   window.scrollTo(0, 0);
 
   if (window.location.hash) {
-    history.replaceState(null, '', window.location.pathname + window.location.search);
+    history.replaceState(
+      null,
+      "",
+      window.location.pathname + window.location.search
+    );
   }
 });
 
 document.addEventListener("DOMContentLoaded", function () {
-  const zoomImages = document.querySelectorAll('.img-box img, .about-img img, img.projects');
+  const zoomImages = document.querySelectorAll(
+    ".img-box img, .about-img img, img.projects"
+  );
 
-  const observer = new IntersectionObserver((entries) => {
-    entries.forEach(entry => {
-      if (entry.isIntersecting) {
-        const img = entry.target;
-        img.classList.remove('zoom-animate');
-        void img.offsetWidth;
-        img.classList.add('zoom-animate');
-        img.addEventListener('animationend', () => {
-          img.classList.remove('zoom-animate');
-        }, { once: true });
-      }
-    });
-  }, {
-    threshold: 0.5
-  });
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          const img = entry.target;
+          img.classList.remove("zoom-animate");
+          void img.offsetWidth;
+          img.classList.add("zoom-animate");
+          img.addEventListener(
+            "animationend",
+            () => {
+              img.classList.remove("zoom-animate");
+            },
+            { once: true }
+          );
+        }
+      });
+    },
+    {
+      threshold: 0.5,
+    }
+  );
 
-  zoomImages.forEach(img => {
+  zoomImages.forEach((img) => {
     observer.observe(img);
   });
 });
 
 document.addEventListener("DOMContentLoaded", function () {
-  const zoomImages = document.querySelectorAll('.img-box img .article-container article img.icon');
+  const zoomImages = document.querySelectorAll(
+    ".img-box img .article-container article img.icon"
+  );
 
-  const observer = new IntersectionObserver((entries) => {
-    entries.forEach(entry => {
-      if (entry.isIntersecting) {
-        const img = entry.target;
-        img.classList.remove('zoom-article-animate');
-        void img.offsetWidth;
-        img.classList.add('zoom-article-animate');
-        img.addEventListener('animationend', () => {
-          img.classList.remove('zoom-article-animate');
-        }, { once: true });
-      }
-    });
-  }, {
-    threshold: 0.5
-  });
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          const img = entry.target;
+          img.classList.remove("zoom-article-animate");
+          void img.offsetWidth;
+          img.classList.add("zoom-article-animate");
+          img.addEventListener(
+            "animationend",
+            () => {
+              img.classList.remove("zoom-article-animate");
+            },
+            { once: true }
+          );
+        }
+      });
+    },
+    {
+      threshold: 0.5,
+    }
+  );
 
-  zoomImages.forEach(img => {
+  zoomImages.forEach((img) => {
     observer.observe(img);
   });
 });
 
-window.addEventListener('load', () => {
+window.addEventListener("load", () => {
   gsap.from(".home-content h1", {
     duration: 1.2,
     y: 50,
     opacity: 0,
-    ease: "power4.out"
+    ease: "power4.out",
   });
 
   gsap.from(".home-content h3", {
@@ -185,7 +215,7 @@ window.addEventListener('load', () => {
     delay: 0.2,
     y: 30,
     opacity: 0,
-    ease: "power2.out"
+    ease: "power2.out",
   });
 
   gsap.from(".btn-box", {
@@ -193,13 +223,13 @@ window.addEventListener('load', () => {
     delay: 0.4,
     y: 50,
     opacity: 0,
-    ease: "power2.out"
+    ease: "power2.out",
   });
   gsap.to(".img-box", {
     duration: 1.2,
     opacity: 1,
     scale: 1,
-    ease: "power4.out"
+    ease: "power4.out",
   });
 });
 
@@ -209,13 +239,12 @@ document.addEventListener("DOMContentLoaded", function () {
     strings: ["FullStack Developer.", "FrontEnd Enthusiast.", "Tech Explorer."],
     typeSpeed: 45,
     backSpeed: 25,
-    loop: true
+    loop: true,
   });
 });
 
 //Function to animate my sentence in the about section
 document.addEventListener("DOMContentLoaded", function () {
-
   const paragraphContent = `
   My full names are <span class="aboutinfo" data-index="0">Eric Tumu Muheki</span>, a graduate holding a Bachelors Degree in Computer Science from the University of Windsor.<br><br>
   I am enthusiastic about everything software related and would love to apply my skills and knowledge in FullStack development to contribute effectively as a <span class="aboutinfo" data-index="1">Full Stack Developer</span>.<br><br>
@@ -228,28 +257,30 @@ document.addEventListener("DOMContentLoaded", function () {
   let typedStarted = false;
 
   // Create Intersection Observer
-  const observer = new IntersectionObserver((entries) => {
-    entries.forEach(entry => {
-      if (entry.isIntersecting && !typedStarted) {
-        typedStarted = true; // To prevent multiple initializations
-        new Typed("#typed-paragraph", {
-          strings: [paragraphContent],
-          typeSpeed: 20,
-          backSpeed: 0,
-          smartBackspace: false,
-          showCursor: false,
-          loop: false,
-          html: true
-        });
-        observer.unobserve(aboutSection); // This stops observing once started
-      }
-    });
-  }, {
-    threshold: 0.2 // triggers the animation to start when 20% of About section visible
-  });
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting && !typedStarted) {
+          typedStarted = true; // To prevent multiple initializations
+          new Typed("#typed-paragraph", {
+            strings: [paragraphContent],
+            typeSpeed: 20,
+            backSpeed: 0,
+            smartBackspace: false,
+            showCursor: false,
+            loop: false,
+            html: true,
+          });
+          observer.unobserve(aboutSection); // This stops observing once started
+        }
+      });
+    },
+    {
+      threshold: 0.2, // triggers the animation to start when 20% of About section visible
+    }
+  );
 
   observer.observe(aboutSection);
-
 });
 
 //Javascript to handle light and dark mode
